bfb1e93cecd024602f9004ccc3cc6f3c2fa0f925
kierah mcgill <kierahalexis7@gmail.com>
2022-06-29 13:32:20 -0400

    commit tree 10e83034b9f98201efeaffae0b6900e0492a4cd3

---
+++ index.js
@@ -0,0 +1,9 @@
+const heading = document.querySelector("h1");
+
+console.log(heading);
+heading.innerhtml= "Flatiron is the best school!";
+
+const newElement = document.createElement("div");
+console.log(newElement);
+newElement.innerHTML = "<h2>I'm having so much fun now<h2>"
+console.log(newElement);
